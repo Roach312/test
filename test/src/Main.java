063f4d0dfd9381ebe625ef5a6755f2558b3c768e
Vlad <ozerov_vlad@mail.ru>
2023-10-11 22:13:40 +0300

    Added new print to feature

--- test/src/Main.java
+++ test/src/Main.java
@@ -2,5 +2,6 @@ public class Main {
     public static void main(String[] args) {
         System.out.println("LLLLLLLLLLL");
         System.out.println("blabla");
+        System.out.println("Change 1 feature");
     }
 }
